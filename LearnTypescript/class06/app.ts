c2d11cfa125dae55b96484f6a990254e5dfa7259
naveed-rana <rana.naveed812@gmail.com>
2022-08-27 18:56:15 +0500

    class 06

---
+++ LearnTypescript/class06/app.ts
@@ -0,0 +1,126 @@
+// class06
+console.log("lin1");
+console.log("lin2");
+function printLine3() {
+  console.log("line 3");
+}
+console.log("lin4");
+console.log("lin5");
+printLine3()
+console.log("lin6");
+console.log("lin7");
+printLine3()
+
+// write a ts add function, should be able to print addition of two numbers
+function sumTwoNumbers(num1: number, num2:number){
+  // var number1:number = 10
+  // var number2:number = 20
+  var result: number = num1 + num2
+  console.log("result=", result);
+}
+
+sumTwoNumbers(5,10)
+
+sumTwoNumbers(10,20)
+
+
+sumTwoNumbers(30,30)
+
+console.log();
+// parseInt()
+// Number()
+// parseFloat()
+
+// while(index < 2){
+// console.log("MAD - Batch1");
+// console.log("MAD - Batch2");
+// index = index + 1
+// }
+// console.log("line12");
+
+
+// // var index = 0;
+// // index < 10;
+// // index++;
+// // write a ts program than can print table of 2;
+// var requiredName:string = "Naveed"
+// var requiredNameIsFound:boolean = false;
+
+// for ( var i = 1; i <= 50 ; i++){
+  
+//   let result:string = `5 * ${i} = ${5 * i}` 
+//   // requiredNameIsFound = true
+// console.log(result);
+// console.log(result);
+// console.log(result);
+// console.log(result);
+// if (i == 15) {
+//   continue;
+// }
+
+// console.log(result);
+// console.log(result);
+
+// console.log(result);
+
+// console.log(result);
+
+// console.log(result);
+// console.log(result);
+
+// console.log(result);
+// console.log(result);
+
+// // if (requiredNameIsFound) {
+//   if(i == 20){
+//   break;
+// }
+// // console.log("2 * " +  i + ' = ' + 2 * i);
+// }
+
+
+// var num:number = 1
+// do {
+//  console.log(`MAD - Batch${num}`);
+
+// num++
+// }while(num <= 5)
+
+
+
+for (let index = 0; index < 10; index++) {
+  console.log("Mad");
+  // if (i == 5) {
+  //   continue;
+  // }
+  console.log(`Batch${index}`);
+}
+
+
+var userValue:number = parseInt(prompt("Enter your Vale") || 0)
+
+if (userValue <= 10000) {
+  // percent? 20%
+}
+
+else if(userValue <= 20000){
+// percent? 30%
+}
+
+else {
+// percent? 30%
+}
+
+
+
+
+
+// // {
+// //   var userName:string = "Naveed"
+// //   console.log("MAD - Batch1");
+// //   console.log("MAD - Batch2");
+// //   console.log("MAD - Batch3");
+// //   console.log("MAD - Batch4");
+// //   console.log("MAD - Batch5");
+// //   }
+  
