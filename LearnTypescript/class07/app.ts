f5553037f1bce5881771dff878d69ae2b6cb07b0
naveed-rana <rana.naveed812@gmail.com>
2022-09-04 18:24:10 +0500

    class07

---
+++ LearnTypescript/class07/app.ts
@@ -0,0 +1,25 @@
+function addTwoNumbers( num1:number = 15, num2:number = 10) : number {
+  var result = num1 + num2
+  console.log("result,", result);
+  return result
+  
+  
+}
+var store:number =  addTwoNumbers()
+
+var num1: number = 15
+
+// for (let index = 0; index < 10; index++) {
+//   if (index == 5) {
+//     addTwoNumbers()
+//   }
+
+  
+
+// }
+
+
+// Write a ts program to find maximum and minimum between four numbers(2 can be optional) using functions.
+// Write a ts program to check whether a number is even or odd using functions.
+
+
---
+++ LearnTypescript/class08/app.ts
@@ -0,0 +1,82 @@
+var fullName0:string = "ali"
+var fullName1:string = "Naveed"
+var fullName2:string = "Zain"
+var fullName3:string = "Muhammad"
+var fullName4:string = "Umar"
+var fullName5:string = "Naveed"
+var fullName6:string = "Naveed"
+var fullName7:string = "Naveed"
+var fullName8:string = "Naveed"
+var fullName9:string = "Naveed"
+var fullName11:string = "Naveed"
+var fullName12:string = "Naveed"
+var fullName13:string = "Naveed"
+var fullName14:string = "Naveed"
+
+// var fullNames = ["ali", "Naveed", "umar", "naveed sarwar", "something"]
+// console.log("fullNames",fullNames);
+
+// fullNames[0]
+// console.log("fullNames[0]",fullNames[0]);
+
+// fullNames[1]
+// console.log("fullNames[1]",fullNames[1]);
+// fullNames[2]
+// fullNames[3]
+// fullNames[4]
+// console.log("fullNames[4]",fullNames[4]);
+
+// for (let index = 0; index < 100; index++) {
+//   if(fullNames[index] == "naveed"){
+
+//   }
+//   console.log("array print by index", fullNames[0]);
+// }
+// console.log("length of array", fullNames.length);
+
+// var numbers:number[] = new Array(5)
+
+// numbers[0] = 1
+
+// numbers[500] = 5
+// fullName0.match('/a-z/')
+
+// var userNames:string[]
+// // console.log("userNames", userNames.length);
+// userNames = ["naveed", "ali", "something"]
+// console.log("userNames", userNames.length);
+
+
+// var userNames:string[] = new Array(5)
+// console.log("userNames", userNames.length);
+// userNames[0] = "ali"
+// userNames[1] = "ali"
+// userNames[2] = "ali"
+// userNames[3] = "ali"
+// userNames[4] = "ali"
+// userNames[5] = "ali"
+// console.log("userNames", userNames.length);
+
+
+// push
+
+var studentMarks:number[] = [150,100,200,400]
+console.log("studentMarks",studentMarks);
+// studentMarks.push(130)
+// console.log("studentMarks after push",studentMarks);
+// studentMarks.pop()
+// studentMarks.pop()
+// console.log("studentMarks after pop",studentMarks)
+// studentMarks.unshift(148)
+// console.log("studentMarks after unshift",studentMarks)
+// studentMarks.shift()
+// console.log("studentMarks after shift",studentMarks)
+studentMarks.splice(2,1,350)
+// studentMarks.splice(0,1)
+// studentMarks.splice(0,0, 350)
+// studentMarks.splice(4,0, 450)
+// studentMarks.splice(3,1)
+console.log("studentMarks after splice",studentMarks);
+
+// write a ts function named sayGratitude that take input of student list, and say welcome to university e.g
+// Hi, Naveed Welcome to university 
