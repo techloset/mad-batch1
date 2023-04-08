5099dee23f1e98f3173a893a687562759aa369d0
Naveed Trainer <learnwithnaveedsarwar>
2023-04-08 13:21:19 +0500

    class nodejs

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,38 +1,67 @@
 const express = require('express')
+var bodyParser = require('body-parser')
+
 const app = express()
 const port = 8000
 
+var posts = []
 
-app.get('/getPosts', (req, res) => {
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// // parse application/json
+app.use(bodyParser.json())
+
+app.use((req,res,next)=>{
+ console.log("console in app.use");
+//  res.end("response end in app.use")
+next()
+})
+
+app.use((req,res,next)=>{
+  console.log("console in app.use second");
+ //  res.end("response end in app.use")
+ next()
+ })
+ 
+
+app.post('/createPost', (req,res)=>{
+  console.log("req.body", req.body);
+  posts = [...posts, req.body]
+  res.send("post req response")
+})
+
+app.get('/getPosts',(req, res) => {
   // get posts
   console.log("req", req.query.uid);
   try {
     
-    var posts = [{
-      id: 1,
-      description: 'safdas asdf ',
-      createdAt: new Date(),
-      postOs: 'web'
-    },
-    {
-      id: 2,
-      description: 'safdas asdf ',
-      createdAt: new Date(),
-      postOs: 'web'
-    },
-    {
-      id: 3,
-      description: 'safdas asdf ',
-      createdAt: new Date(),
-      postOs: 'web'
-    }
-    ]
+    // var posts = [{
+    //   id: 1,
+    //   description: 'safdas asdf ',
+    //   createdAt: new Date(),
+    //   postOs: 'web'
+    // },
+    // {
+    //   id: 2,
+    //   description: 'safdas asdf ',
+    //   createdAt: new Date(),
+    //   postOs: 'web'
+    // },
+    // {
+    //   id: 3,
+    //   description: 'safdas asdf ',
+    //   createdAt: new Date(),
+    //   postOs: 'web'
+    // }
+    // ]
+  
 
-    let requiredPost = posts.filter((post)=> post.id == req.query.id)
+    // posts = posts.filter((post)=> post.id == req.query.id)
 
     res.status(200).json({
       message: 'sucess',
-      data: requiredPost
+      data: posts
     })
 
   } catch (error) {
