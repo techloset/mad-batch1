b13ee32059b876af94c1e6d84c29df3efa85a719
Naveed Trainer <learnwithnaveedsarwar>
2023-04-30 20:27:25 +0500

    nodejs class

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,9 +1,25 @@
 const express = require('express')
 var bodyParser = require('body-parser')
 var cors = require('cors')
+const mongoose = require('mongoose');
 
 
-const multer  = require('multer')
+main()
+
+async function main() {
+  try {
+   const connection =  await mongoose.connect('mongodb+srv://admin:test1214@cluster0.g17dot3.mongodb.net/test');
+     console.log("connection successful",connection);
+  } catch (error) {
+    console.log(
+      "connection error", error
+    );
+  }
+
+}
+
+
+const multer = require('multer')
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, 'public')
@@ -12,7 +28,7 @@ const storage = multer.diskStorage({
     cb(null, file.originalname)
   }
 })
-const upload = multer({storage: storage})
+const upload = multer({ storage: storage })
 
 
 const app = express()
@@ -38,8 +54,53 @@ var posts = [{
 }
 ]
 
+// const postSchema = new mongoose.Schema({
+//   address: {
+//     city: {
+//       type: String,
+//       required:true,
+//     },
+//     state: String,
+//     country: String
+//   },
+//   email:{
+//     type: String,
+//     validate: (email)=> true,
+//     unique: true
+//   },
+//   description: String,
+//   age: {
+//     type: Number,
+//     min: 15,
+//     max: 65,
+//     required: true,
+//   },
+//   createdAt: {
+//     type: Date,
+//     default: Date.now,
+//     required: true,
+//   },
+//   postOs: String
+// });
 
- 
+
+const postSchema = new mongoose.Schema({
+  description: String,
+  age: {
+    type: Number,
+    min: 15,
+    max: 65,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  postOs: String
+});
+
+const postsCollection = mongoose.model('posts', postSchema);
+
 app.use(cors())
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -47,26 +108,26 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // // parse application/json
 app.use(bodyParser.json())
 
-app.use((req,res,next)=>{
- console.log("console in app.use");
-//  res.end("response end in app.use")
-next()
+app.use((req, res, next) => {
+  console.log("console in app.use");
+  //  res.end("response end in app.use")
+  next()
 })
 
-app.use((req,res,next)=>{
+app.use((req, res, next) => {
   console.log("console in app.use second");
- //  res.end("response end in app.use")
- next()
- })
- 
+  //  res.end("response end in app.use")
+  next()
+})
 
- app.use(express.static('public'))
+
+app.use(express.static('public'))
 // react > createPost> storage > db
 
 
 // app.post('/createPost',upload.single('avatar'),(req,res)=>{
 //   console.log("req.body", req.file);
-  
+
 //   posts = [...posts, {...req.body,imageURL: req.file.path}]
 //   res.status(200).json({
 //     message: 'sucess',
@@ -74,24 +135,40 @@ app.use((req,res,next)=>{
 //   })
 // })
 
-app.post('/createPost',(req,res)=>{
-
+app.post('/createPost', async(req, res) => {
+try {
   console.log('====================================');
   console.log(".req.body", req.body);
   console.log('====================================');
-  
-  posts = [...posts, {...req.body}]
+  const post = new postsCollection(req.body);
+  const result = await post.save()
+  console.log("result", result);
+  const posts = await postsCollection.find();
+  console.log("posts", posts);
+  // posts = [...posts, { ...req.body }]
   res.status(200).json({
     message: 'sucess',
     data: posts
   })
+} catch (error) {
+  console.log("error",error);
+  res.status(500).json({
+    message: 'failed',
+    error:error,
+    data: []
+  })
+}
+ 
 })
 
 
-app.delete('/deletePost', (req,res)=>{
-  console.log("req.query", req.query.id);
-  const idValue =  parseInt(req.query.id)
-  posts = posts.filter((post)=> post.id !== idValue)
+app.delete('/deletePost', async(req, res) => {
+  try {
+    console.log("req.query", req.query.id);
+  const idValue = parseInt(req.query.id)
+  
+  // posts = posts.filter((post) => post.id !== idValue)
+  const posts = await postsCollection.deleteOne({postOs:req.query.id})
   console.log('====================================');
   console.log("posts", posts);
   console.log('====================================');
@@ -99,17 +176,31 @@ app.delete('/deletePost', (req,res)=>{
     message: 'sucess',
     data: posts
   })
+  } catch (error) {
+    res.status(500).json({
+      message: 'failed',
+      error:error,
+      data: []
+    })
+  }
+  
 })
 
 
-app.get('/getPosts',(req, res) => {
+app.get('/getPosts', async(req, res) => {
   // get posts
   console.log("req", req.query.uid);
   try {
-    
-    
 
+    let posts = []
+      if (req.query.uid) {
+        posts = await postsCollection.find({postOs:  req.query.uid});
+      }
+      else {
+        posts = await postsCollection.find();
+      }
     // posts = posts.filter((post)=> post.id == req.query.id)
+   
 
     res.status(200).json({
       message: 'sucess',
